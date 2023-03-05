e6fd3c898f7b691ba8f8f85120e5015aa56a0735
cthegost <akhavkin@medlinesoft.ru>
2023-03-05 14:02:44 +0300

    fix

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -6,7 +6,7 @@ import icon from "../images/emoji.svg";
 import EmojiPicker from "emoji-picker-react";
 import { Messages } from "./Messages";
 
-const socket = io.connect("http://localhost:5000");
+const socket = io.connect("https://server-chat-nrkj.onrender.com");
 
 export const Chat = () => {
   const { search } = useLocation();
